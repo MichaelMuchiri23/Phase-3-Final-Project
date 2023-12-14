f03b95eb3c960bc61a5ec42429b35e57bad48f79
Everlyn <gachuieve32@gmail.com>
2023-12-14 09:48:35 +0300

    migrations

---
+++ lib/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey, Column, Integer, String
+# from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///todos.db')
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    def __repr__(self):
+        return f'Category(id={self.id}, ' + \
+            f'name={self.name})'
+    
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer(), primary_key=True)
+    tag_name = Column(String())
+
+    def __repr__(self):
+        return f'Tag(id={self.id},' + \
+            f'tag_name={self.tag_name})'
+    
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    description = Column(String())
+
+    category_id = Column(Integer(), ForeignKey('categories.id'))
+    tag_id = Column(Integer(), ForeignKey('tags.id'))
+
+    def __repr__(self):
+        return f'Task(id={self.id},' + \
+            f'title={self.title},' + \
+            f'description{self.description},' + \
+            f'category_id={self.category_id},' + \
+            f'tag_id={self.tag_id})'
